5e7c4c01ce2db39f320b4dbd9bbe3094f9d6c17e
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-15 21:27:26 +0530

    snake component

---
+++ src/Snake.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Snake = (props) => {
+
+ 
+    return (
+        <div>
+            {props.snakeDots.map((dot,i)=>{
+                   const style = {
+                       left: `${dot[0]}%`,
+                       top: `${dot[1]}%`
+
+                    }
+                
+                return(
+                    <div className="snake-dot" key={i} style={style}>
+
+                    </div>
+                )
+            })}
+        </div>
+    )
+}

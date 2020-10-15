4296546e30c1c153093d177d691905a00dd351eb
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-15 21:49:51 +0530

    generated random coordinates to place the food at random postions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, {Component} from "react";
 import { Snake } from "./Snake";
 import { SnakeFood } from "./SnakeFood";
 
-
+const  generateRandomCoordinates =()=>{
+  let min = 1;
+  let max= 98;
+  let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  return [x,y]
+}
 
 
 export default class App extends Component {
   state ={
-    food:[6,8],
+    food:generateRandomCoordinates(),
     snakeDots:[
       [0,0],
       [2,0] 

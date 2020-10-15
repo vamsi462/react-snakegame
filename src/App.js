953868ca76c113e91acbad3111f6dd0c36cc57be
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-15 21:28:50 +0530

    imported Snake component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { Snake } from "./Snake";
 
 
 
@@ -13,9 +14,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="game-area">
-        <div className="snake-dot" style={{top: 0, left: 0}}></div>
-        <div className="snake-dot" style={{top: 0, left: '2%'}}></div>
-        <div className="snake-dot" style={{top: 0, left: '4%'}}></div>
+        <Snake/>
       </div>
     );
   }

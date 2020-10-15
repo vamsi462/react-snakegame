5521eaa0d558131e0706b260f7e71348722b7344
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-15 21:10:27 +0530

    shaped out the snake

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import React, {Component} from "react";
 export default class App extends Component {
   render() {
     return (
-    <div className="game-area">
-       <div className="snake-dot"></div>
-    </div>
+      <div className="game-area">
+        <div className="snake-dot" style={{top: 0, left: 0}}></div>
+        <div className="snake-dot" style={{top: 0, left: '2%'}}></div>
+        <div className="snake-dot" style={{top: 0, left: '4%'}}></div>
+      </div>
     );
   }
 }

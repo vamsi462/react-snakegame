1d1af59a8e3da8b427f65a7286630eaabe900670
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-15 21:04:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+    <div className="game-area">
+       <div className="snake-dot"></div>
+    </div>
+    );
+  }
+}
